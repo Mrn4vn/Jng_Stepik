d4430d12f53e3880b88f420394e2b308b4be5f26
Mrn4vn <70034865+Mrn4vn@users.noreply.github.com>
2024-09-29 10:52:42 +0300

    added files into a new dir library
    added files into a new dir library

---
+++ library/views.py
@@ -0,0 +1,133 @@
+import io
+
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import mixins
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, renderer_classes, action
+from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView, ListAPIView, RetrieveAPIView
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework.parsers import JSONParser
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAuthenticatedOrReadOnly, IsAdminUser, \
+    BasePermission, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
+from .models import Author, Book
+from .serializers import AuthorModelSerializer, AuthorSerializer, BookModelSerializer, BookSerializer, \
+    AuthorModelSerializerV2
+
+
+class CustomPermissions(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user and request.user.username == 'denis'
+
+
+class AuthorLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 2
+
+
+class AuthorModelViewSet(ModelViewSet):
+    # pagination_class = AuthorLimitOffsetPagination
+    # permission_classes = [DjangoModelPermissions]
+    # serializer_class = AuthorModelSerializer
+    queryset = Author.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.version == '2.0':
+            return AuthorModelSerializerV2
+        return AuthorModelSerializer
+
+    @action(detail=True, methods=['get'])
+    def get_author_name(self, request, pk):
+        author = get_object_or_404(Author, pk=pk)
+        return Response({'name': str(author)})
+
+    def get_queryset(self):
+        first_name = self.request.query_params.get('first_name', None)
+        if first_name:
+            return Author.objects.filter(first_name__containt=first_name)
+        return Author.objects.all()
+
+
+class BookModelViewSet(ModelViewSet):
+    serializer_class = BookModelSerializer
+    queryset = Book.objects.all()
+
+
+class BookModelLimitedViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    mixins.UpdateModelMixin,
+    GenericViewSet
+):
+    serializer_class = BookModelSerializer
+    queryset = Book.objects.all()
+
+
+class BookApiView(APIView):
+    renderer_classes = [JSONRenderer]
+
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
+
+class BookListAPIView(ListAPIView):
+    renderer_classes = [JSONRenderer]
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+
+@api_view(['GET'])
+@renderer_classes([JSONRenderer])
+def book_api_get(request):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
+
+
+def book_get(request):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+
+    json_data = JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data)
+
+
+def author_get(request, pk=None):
+    if pk is not None:
+        author = Author.objects.get(pk=pk)
+        serializer = AuthorSerializer(author)
+    else:
+        authors = Author.objects.all()
+        serializer = AuthorSerializer(authors, many=True)
+
+    json_data = JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data)
+
+
+@csrf_exempt
+def author_post(request, pk=None):
+    json_data = JSONParser().parse(io.BytesIO(request.body))
+
+    if request.method == 'POST':
+        serializer = AuthorSerializer(data=json_data)
+    elif request.method == 'PUT':
+        author = Author.objects.get(pk=pk)
+        serializer = AuthorSerializer(author, data=json_data)
+    elif request.method == 'PATCH':
+        author = Author.objects.get(pk=pk)
+        serializer = AuthorSerializer(author, data=json_data, partial=True)
+
+    if serializer.is_valid():
+        author = serializer.save()
+
+        serializer = AuthorSerializer(author)
+        json_data = JSONRenderer().render(serializer.data)
+        return HttpResponse(json_data)
+
+    return HttpResponseBadRequest(JSONRenderer().render(serializer.errors))
